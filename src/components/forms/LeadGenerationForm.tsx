2dca7ab2815fee59b38a477b87e4c3ba93d71e69
Piyushkr001 <piyushthegreat2305@gmail.com>
2024-11-04 12:44:33 +0530

    almost done form

--- src/components/forms/LeadGenerationForm.tsx
+++ src/components/forms/LeadGenerationForm.tsx
@@ -89,7 +89,6 @@ export default function LeadGenerationForm() {
                 </FormItem>
               )}
             />
-            {errors.phone && <p className="text-red-500 text-sm mt-1">{errors.phone}</p>}
           </div>
 
           <div className="flex flex-col sm:flex-row sm:space-x-4">
@@ -171,5 +170,5 @@ export default function LeadGenerationForm() {
         </form>
       </Form>
     </div>
-  );
+  )
 }
