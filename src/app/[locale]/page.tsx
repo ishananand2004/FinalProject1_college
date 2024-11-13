2aa0fb22c42d0f59fdc0ce1693041a25762a84c1
Kaustubh Agrawal <kaustubhkagrawal@gmail.com>
2024-11-13 08:59:48 +0530

    fix intl

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,12 +1,15 @@
-import {useTranslations} from 'next-intl';
-import {Link} from '@/i18n/routing';
- 
+import { useTranslations } from "next-intl";
+import { Link } from "@/i18n/routing";
+
 export default function HomePage() {
-  const t = useTranslations('HomePage');
+  const t = useTranslations("HomePage");
+
+  console.log("home page rendered", t("title"));
+
   return (
     <div>
-      <h1>{t('title')}</h1>
-      <Link href="/about">{t('about')}</Link>
+      <h1>{t("title")}</h1>
+      <Link href="/about">{t("about")}</Link>
     </div>
   );
 }
